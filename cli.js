7f38691bb1ccade8916088bb0c0685aae184eef3
httpjamesm <github@httpjames.space>
2021-06-09 20:43:42 -0400

    exit command, package json, env file, redo socketio

--- cli.js
+++ cli.js
@@ -1,142 +1,109 @@
+// Imports
+
 const CryptoJS = require('crypto-js');
 const io = require('socket.io-client');
 const readline = require('readline');
+const dotenv = require('dotenv').config();
 
-const protocol = 'https://';
-const hostname = 'cryptochat.dev';
-const port = '443';
+const lineReader = readline.createInterface({input: process.stdin, output: process.stdout});
 
-const lineReader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-var username = '';
-var password = '';
+var username;
+var password;
+var roomName;
 
 // connect to socketio endpoint
-var socket = io.connect(protocol + hostname + ':' + port);
-
-socket.on('connect', function () {
-    // on connect
-    socket.emit('chat event', {
-        data: 'User Connected'
-    });
-});
+let socket;
+const initiateSocket = (room) => {
+    socket = io(process.env.API);
+    var roomName = CryptoJS.SHA512(room).toString();
+    socket.emit('join', roomName)
+}
 
 getUsername();
 
 function getUsername() {
-        lineReader.question('What is your username? ', (answer) => {
-                username = answer;
-                getPassword();
-        });
-}
-
-function getPassword() {
-        lineReader.question('What is your room password? ', (answer) => {
-                password = answer;
-
-                socket.emit('chat event', {
-                    "user_name": code.encryptMessage(username, password),
-                    "message": code.encryptMessage('has joined the room.', password)
-                });
-
-                chat();
-        });
-}
-
-function chat() {
-        lineReader.question('Send a message: ', (answer) => {
-                checkCommands(answer);
-                chat();
-        });
-}
-
-function checkCommands(message) {
-    var args = message.split(' ');
-
-    // switch for command
-    switch (args[0]) {
-        case '/nick':
-            // change username
-            if (args.length == 1) {
-                // if no username arguments were provided then alert the user
-                console.log('Invalid nickname. Correct usage: /nick <username>');
-
-            } else {
-                username = args[1];
-
-                // broadcast the username change to the whole room
-                socket.emit('chat event', JSON.parse({
-                    "user_name": code.encryptMessage(username, password),
-                    "message": code.encryptMessage('changed their username to ' + username, password)
-                }));
-            }
-
-            break;
-
-        case '/leave':
-            // leave the room
-            leaveAndReload();
-            break;
-
-        default:
-            // if no slash command, just send the message normally
-            sendMessage(message);
-    }
-}
-
-let code = {
-        encryptMessage: function (messageToEncrypt = '', secretkey = '') {
-                var encryptedMessage = CryptoJS.AES.encrypt(
-                        messageToEncrypt,
-                        secretkey
-                );
-
-                return encryptedMessage.toString();
-        },
-        decryptMessage: function (encryptedMessage = '', secretkey = '') {
-                var decryptedBytes = CryptoJS.AES.decrypt(
-                        encryptedMessage,
-                        secretkey
-                );
-
-                return decryptedBytes.toString(CryptoJS.enc.Utf8);
-        }
-};
-
-function sendMessage(message) {
-    socket.emit('chat event', {
-        // encrypt and send the user's name and message
-        "user_name": code.encryptMessage(username, password),
-        "message": code.encryptMessage(message, password)
+    lineReader.question('Username: ', (answer) => {
+        username = answer;
+        getPassword();
     });
 }
 
-socket.on('my response', function (msg) {
-    // console.log(msg); // for debugging: print the encrypted contents of the response
+function getPassword() {
+    lineReader.question('Room Key: ', (answer) => {
+        password = answer;
+        roomName = CryptoJS.SHA512(password).toString();
+        initiateSocket(roomName)
 
-    if (typeof msg.user_name !== 'undefined') {
-        if (code.decryptMessage(msg.user_name, password) == '' && code.decryptMessage(msg.message, password) == '') {
-            // if the username and key are empty values, stop
+        socket.emit('chat event', {
+            "roomName": roomName,
+            "user_name": crypt.encryptMessage(username, password),
+            "message": crypt.encryptMessage('has joined the room.', password)
+        });
+
+        socket.on('my response', incomingHandler);
+        chat();
+    });
+}
+
+
+function chat() {
+    lineReader.question(`${username}: `, (answer) => {
+        if (answer == '/exit') {
+            leaveAndReload();
             return;
         }
+        if (answer.length > 0) {
+            sendMessage(answer);
+        }
+        chat();
+    });
+}
 
-        console.log(decodeURI(code.decryptMessage(msg.user_name, password)) + ': ' + code.decryptMessage(msg.message, password).toString());
+let crypt = {
+    encryptMessage: function (messageToEncrypt = '', secretkey = '') {
+        var encryptedMessage = CryptoJS.AES.encrypt(messageToEncrypt, secretkey);
+
+        return encryptedMessage.toString();
+    },
+    decryptMessage: function (encryptedMessage = '', secretkey = '') {
+        var decryptedBytes = CryptoJS.AES.decrypt(encryptedMessage, secretkey);
+
+        return decryptedBytes.toString(CryptoJS.enc.Utf8);
     }
-});
+};
 
-function leaveRoom() {
-    // on tab close, broadcast to the room
+function sendMessage(message) {
+    socket.emit('chat event', { // encrypt and send the user's name and message
+        "roomName": roomName,
+        "user_name": crypt.encryptMessage(username, password),
+        "message": crypt.encryptMessage(message, password)
+    });
+}
+
+
+function incomingHandler(msg) {
+    var decryptedUsername;
+    var decryptedMessage;
+    decryptedUsername = crypt.decryptMessage(msg.user_name, state.key);
+    decryptedMessage = crypt.decryptMessage(msg.message, state.key);
+    if (decryptedUsername !== '' || decryptedMessage !== '') { // if the username and message are empty values, stop
+        console.log(`${decryptedUsername}: ${decryptedMessage}`);
+    } else {
+        var time = new Date();
+        console.log(`[${time}] Could not decrypt: ${msg}`);
+    }
+}
+
+function leaveRoom() { // on tab close, broadcast to the room
 
     socket.emit('chat event', JSON.parse({
-        "user_name": code.encryptMessage(username, password),
-        "message": code.encryptMessage('has left the room.', password)
+        "roomName": roomName,
+        "user_name": crypt.encryptMessage(username, password),
+        "message": crypt.encryptMessage('has left the room.', password)
     }));
 }
 
 function leaveAndReload() {
-        leaveRoom();
-        process.exit(0);
+    leaveRoom();
+    process.exit(0);
 }
